ac4d204f4a6ce81a5d8a559a0b77684c238b9346
truongnat <truongdq.dev@gmail.com>
2025-07-29 21:03:06 +0700

    feat: Setup RootLayout and move global styles

---
+++ app/layout.tsx
@@ -0,0 +1,9 @@
+import '../styles/globals.css';
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className="bg-gray-900">{children}</body>
+    </html>
+  );
+}
